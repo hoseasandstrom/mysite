d3b702f0005ad18f297206b8d22cd804ffaab2a9
hoseasandstrom <Lakyn4ever>
2016-09-16 12:46:26 -0400

    Started adding packages

---
+++ src/main/java/com/sandstromhosea/controllers/SiteController.java
@@ -0,0 +1,14 @@
+package com.sandstromhosea.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by hoseasandstrom on 9/16/16.
+ */
+@RestController
+public class SiteController {
+
+}
